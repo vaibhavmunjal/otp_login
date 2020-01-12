8664753a64bc0f585cc106efb6dd586d155107d2
vaibhavmunjal <vaibhavmunjal17@gmail.com>
2020-01-12 09:44:26 +0530

    otp model and backend created

--- otp_login/account/models.py
+++ otp_login/account/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+User = get_user_model()
+
+class OTPUser(models.Model):
+    """
+    otp model to authenticate
+    """
+
+    user = models.OneToOneField(User,
+                            verbose_name=_("User"),
+                            on_delete=models.CASCADE,
+                            related_name='otp')
+
+    otp = models.CharField(_("OTP"),
+                        max_length=6,
+                        blank=True,
+                        null=True)
+
+    @property
+    def generate_otp(self):
+        return User.objects.make_random_password(length=6, allowed_chars='0123456789')
--- otp_login/otp_login/settings.py
+++ otp_login/otp_login/settings.py
@@ -121,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'account.backends.OTPModelBackEnd',
+)
